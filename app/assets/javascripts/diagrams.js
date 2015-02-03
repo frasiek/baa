f265bde3a868a226d08f2321958e7db51a5f6f57
Michał Fraś <michal@frasiek.com>
2015-02-03 21:24:02 +0100

    skonczone raporty

--- app/assets/javascripts/diagrams.js
+++ app/assets/javascripts/diagrams.js
@@ -3,13 +3,139 @@
  */
 //= require excanvas.min
 //= require jquery.jqplot.min
+//= require plugins/jqplot.canvasAxisLabelRenderer.min.js
+//= require plugins/jqplot.canvasTextRenderer.min.js
+//= require plugins/jqplot.highlighter.min.js
+//= require plugins/jqplot.cursor.min.js
+//= require plugins/jqplot.dateAxisRenderer.min.js
+//= require plugins/jqplot.enhancedLegendRenderer.min.js
+//= require plugins/jqplot.pieRenderer.min.js
 
-$(document).ready(function(){
-    $("#filters").submit(function(e){
+$(document).ready(function () {
+    var diagramObj = null
+    $("#filters").submit(function (e) {
         e.preventDefault();
         var type = $("[data-type]").attr("data-type");
-        $.post("/report/data.json",{type:type, filters: $(this).serialize()},function(response){
-            console.log(response)
-        },'json');
+        $.post("/report/data.json", {type: type, filters: $(this).serialize()}, function (response) {
+            if (diagramObj === null) {
+                diagramObj = new diagram(type, response);
+            } else {
+                diagramObj.setData(response)
+            }
+            diagramObj.draw();
+        }, 'json');
     });
 });
+
+function diagram(type, data) {
+    this.type = type;
+    this.data = data;
+}
+
+diagram.prototype.setData = function (data) {
+    this.data = data;
+}
+
+diagram.prototype.clear = function (data) {
+    $("#chart").html("");
+}
+
+diagram.prototype.draw = function () {
+    this.clear();
+    var data = null;
+    if (this.type == 'line') {
+        data = this.prepareDataLine();
+    } else {
+        data = this.prepareDataPie();
+    }
+    this._draw(data[0], data[1]);
+}
+
+diagram.prototype.prepareDataLine = function () {
+    var series = new Array();
+    var series_titles = new Array();
+    var return_array = new Array();
+    $.each(this.data, function (indx, item) {
+        if (typeof series[item[1]] != "object") {
+            series_titles.push({
+                "label": item[1],
+                lineWidth: 4
+            });
+            series[item[1]] = new Array();
+        }
+        series[item[1]].push([item[2], item[0] * 1]);
+    });
+    $.each(series_titles, function (i, item) {
+        return_array.push(series[item.label]);
+    });
+    return [return_array, series_titles];
+}
+
+diagram.prototype.prepareDataPie = function () {
+    var series = new Array();
+    var series_titles = new Array();
+    $.each(this.data, function (indx, item) {
+        var title = item[1];
+        var value = item[0]*1;
+        var set = false;
+        $.each(series, function(i, item){
+            if(item[0] == title){
+                series[i][1]+=value;
+                set = true;
+            }
+        });
+        if(!set){
+            series.push([title, value]);
+        }
+    });
+    return [series, []];
+}
+
+diagram.prototype._draw = function (data, series) {
+    if (this.type == 'line') {
+        this.plot = $.jqplot("chart", data, {
+            series: series,
+            axes: {
+                xaxis: {
+                    renderer: $.jqplot.DateAxisRenderer,
+                    tickOptions: {
+                        formatString: '%b&nbsp;%#d'
+                    }
+                },
+                yaxis: {
+                    tickOptions: {
+                        formatString: '%.2fzł'
+                    }
+                }
+            },
+            highlighter: {
+                show: true,
+                sizeAdjust: 7.5
+            },
+            cursor: {
+                show: false,
+                tooltipLocation:'sw',
+                zoom:true
+            },
+            legend: {
+                renderer: $.jqplot.EnhancedLegendRenderer,
+                show: true
+            }
+        });
+    } else {
+        this.plot = $.jqplot ('chart', [data],
+            {
+                seriesDefaults: {
+                    // Make this a pie chart.
+                    renderer: jQuery.jqplot.PieRenderer,
+                    rendererOptions: {
+                        // Put data labels on the pie slices.
+                        // By default, labels show the percentage of the slice.
+                        showDataLabels: true
+                    }
+                },
+                legend: { show:true, location: 'e' }
+            }
+        );
+    }
+}
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -19,11 +19,25 @@ class Operation < ActiveRecord::Base
       categories << connection.quote(value)
     end
     categories_id = categories.join ", "
-    sql = "select distinct o.id,  sum(o.`value`) val, GROUP_CONCAT(c.name) categories, o.accounting_date from operations o
-      join categories c on o.details_recipient like concat('%',c.keyword,'%')
-      where c.name in (select distinct name from categories where id in ("+categories_id+")) and o.user_id = "+connection.quote(user_id)+"
-      and o.accounting_date >= "+connection.quote(filters['date_from'][0])+" and o.accounting_date <= "+connection.quote(filters['date_to'][0])+"
-      group by o.accounting_date, o.id"
+
+    sql = "SELECT abs(sum(value)) v, categorie, currency_date from (
+      SELECT operations.*, GROUP_CONCAT(DISTINCT categories.name  ORDER BY categories.name SEPARATOR ', ') categorie FROM operations
+      join categories on name in (
+          SELECT name FROM categories
+          WHERE id in ("+categories_id+")
+        ) and details_recipient like concat('%',keyword,'%')
+      WHERE operations.user_id = "+connection.quote(user_id)+" and operations.currency_date >= "+connection.quote(filters['date_from'][0])+"
+        and operations.currency_date <=  "+connection.quote(filters['date_to'][0])+"
+      group by id
+      ) tmp
+      group by categorie, currency_date"
+
+    # sql = " select sum(val) val, categories, currency_date from (select distinct o.id,  -sum(o.`value`) val,
+    #     GROUP_CONCAT(DISTINCT c.name  ORDER BY c.name SEPARATOR ', ') categories, o.currency_date from operations o
+    #   join categories c on o.details_recipient like concat('%',c.keyword,'%')
+    #   where c.name in (select distinct name from categories where id in ("+categories_id+")) and o.user_id = "+connection.quote(user_id)+"
+    #   and o.currency_date >= "+connection.quote(filters['date_from'][0])+" and o.currency_date <= "+connection.quote(filters['date_to'][0])+"
+    #   group by o.currency_date) as tmp group by  currency_date, categories"
     return ActiveRecord::Base.connection.execute(sql)
   end
 end
